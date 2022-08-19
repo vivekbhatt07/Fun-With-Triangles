aa180449d75e29598d6da8cda3bb8dca135d4191
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-08-19 20:48:44 +0530

    fix: hypotenuse error handle

--- hypotenuse.js
+++ hypotenuse.js
@@ -4,17 +4,22 @@ const hypotenuseBtn = document.querySelector(".check_hypotenuse_btn");
 const hypotenuseText = document.querySelector(".hypotenuse_text");
 
 hypotenuseBtn.addEventListener("click", function calculateHypotenuse() {
-  const hypotenuse = hypotenuseCalc(
-    perpendicularLength.value,
-    baseLength.value
-  );
-  hypotenuseText.innerText = hypotenuse;
+  if(!(Number(perpendicularLength.value) === 0) && !(Number(baseLength.value) === 0)) {
+    const hypotenuse = hypotenuseCalc(
+      perpendicularLength.value,
+      baseLength.value
+    );
+    hypotenuseText.innerText ="Hypotenuse is : "+ hypotenuse;
+  } else {
+    hypotenuseText.innerText = "Invalid Data Entered";
+  }
+
 });
 
 function hypotenuseCalc(num1, num2) {
   const squareNum1 = Math.pow(Number(num1), 2);
   const squareNum2 = Math.pow(Number(num2), 2);
   const squareSum = squareNum1 + squareNum2;
-  const sumSquareRoot = Math.sqrt(squareSum);
+  const sumSquareRoot = Math.sqrt(squareSum).toFixed(2);
   return sumSquareRoot;
 }
